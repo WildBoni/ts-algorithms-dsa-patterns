173abd050463c4425b03f21dc1635de03c7513ce
WildBoni <web@davidebonifacio.it>
2025-02-04 11:28:37 +0100

    problem 15 solution

--- src/15-three-sum.ts
+++ src/15-three-sum.ts
@@ -28,30 +28,72 @@ Constraints:
 3 <= nums.length <= 3000
 -105 <= nums[i] <= 105
 */
+
+// two sum - like approach (fails with very large arrays)
+// function threeSum(nums: number[]): number[][] {
+//   let result: number[][] = []
+//   let numMap: { [key: string]: number } = {}
+
+//   for (let i = 0; i < nums.length - 1; i++) {
+//     let target = nums[i] * -1
+//     for (let j = i + 1; j < nums.length; j++) {
+//       let complement = target - nums[j]
+//       if (
+//         numMap[complement] &&
+//         numMap[complement] !== i &&
+//         numMap[complement] !== j
+//       ) {
+//         let newTriplet = [nums[i], nums[j], nums[numMap[complement]]]
+//         newTriplet.sort((a, b) => a - b)
+//         if (JSON.stringify(result).indexOf(JSON.stringify(newTriplet)) === -1) {
+//           result.push(newTriplet)
+//         }
+//       } else {
+//         numMap[nums[j]] = j
+//       }
+//     }
+//   }
+//   return result
+// }
+
+// quicker two pointer implementation
 function threeSum(nums: number[]): number[][] {
   let result: number[][] = []
-  let numMap: { [key: string]: number } = {}
-  for (let i = 0; i < nums.length - 1; i++) {
-    let target = nums[i] * -1
-    for (let j = i + 1; j < nums.length; j++) {
-      let complement = target - nums[j]
-      if (
-        numMap[complement] &&
-        numMap[complement] !== i &&
-        numMap[complement] !== j
-      ) {
-        let newTriplet = [nums[i], nums[j], nums[numMap[complement]]]
-        newTriplet.sort((a, b) => a - b)
-        if (JSON.stringify(result).indexOf(JSON.stringify(newTriplet)) === -1) {
-          result.push(newTriplet)
+
+  let sortedNums = nums.sort((a, b) => a - b)
+
+  for (let i = 0; i < sortedNums.length - 2; i++) {
+    if (sortedNums[i - 1] === sortedNums[i]) {
+      continue
+    }
+    let complement: number = sortedNums[i] * -1
+    let j = i + 1
+    let k = sortedNums.length - 1
+
+    while (j < k) {
+      let sum = sortedNums[j] + sortedNums[k]
+      if (sum === complement) {
+        result.push([sortedNums[i], sortedNums[j], sortedNums[k]])
+        j++
+        k--
+        while (sortedNums[j + 1] === sortedNums[j]) {
+          j++
         }
-      } else {
-        numMap[nums[j]] = j
+        while (sortedNums[k - 1] === sortedNums[k]) {
+          k--
+        }
+      } else if (sum > complement) {
+        k--
+      } else if (sum < complement) {
+        j++
       }
     }
   }
+
   return result
 }
-console.log(threeSum([-1, 0, 1, 2, -1, -4]))
+
+// console.log(threeSum([-1, 0, 1, 2, -1, -4]))
 // console.log(threeSum([0, 0, 0]))
 // console.log(threeSum([0, 1, 1]))
+console.log(threeSum([-1, 0, 1, 0]))
